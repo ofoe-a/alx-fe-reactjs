38be3a3fa499e8b10ccd01125e988eb6325ab210
ofoe <ofoe_98@hotmail.co.uk>
2025-08-31 23:04:46 +0000

    modified JSX in prebuilt react compontent

---
+++ alx-react-app/src/components/App.jsx
@@ -0,0 +1,12 @@
+import WelcomeMessage from './components/WelcomeMessage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
