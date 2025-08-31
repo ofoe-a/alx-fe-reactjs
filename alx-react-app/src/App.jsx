b7d9d9b1e2f8e9a66ac4552b970916a24a06c725
ofoe <ofoe_98@hotmail.co.uk>
2025-08-31 23:31:54 +0000

    moved files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,10 @@
+import WelcomeMessage from './components/WelcomeMessage';
 import './App.css';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <MainContent />
-      <Footer />
+      <WelcomeMessage />
     </div>
   );
 }
