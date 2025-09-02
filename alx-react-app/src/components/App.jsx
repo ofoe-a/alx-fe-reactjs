89b0ee0f6b54b98d78e0a6b3f7fb4a16c0038009
ofoe <ofoe_98@hotmail.co.uk>
2025-09-02 15:47:44 +0000

    created UserProfile

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,10 +1,27 @@
+// import './App.css';
+// import WelcomeMessage from './components/WelcomeMessage';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <WelcomeMessage />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
-import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div className="App">
-      <WelcomeMessage />
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
     </div>
   );
 }
