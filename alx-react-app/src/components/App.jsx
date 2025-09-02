a892c09c3c70e56b5af8d03c317a3e159d38d411
ofoe <ofoe_98@hotmail.co.uk>
2025-09-02 20:01:59 +0000

    changes made to App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,16 +1,3 @@
-// import './App.css';
-// import WelcomeMessage from './components/WelcomeMessage';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <WelcomeMessage />
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import './App.css';
 import UserProfile from './components/UserProfile';
 
