dc9693652def27df2f02d1db0b06022b13eb5642
ofoe <ofoe_98@hotmail.co.uk>
2025-09-07 19:00:17 +0000

    Styled Header and User Profile Content

---
+++ alx-react-app-new/src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
---
+++ alx-react-app-new/src/components/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </div>
+  );
+}
+
+export default App;
