1c8a40137fb2c708aa954b47724fbf85b30033f4
ofoe <ofoe_98@hotmail.co.uk>
2025-09-07 20:29:59 +0000

    Refactor Prop drilling Ex

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
+
+function UserProfile() {
+  const user = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p>Age: {user.age}</p>
+      <p>{user.bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
