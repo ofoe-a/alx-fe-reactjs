594b2b48341d38095e3dd3379b2cbfc593cc33eb
ofoe <ofoe_98@hotmail.co.uk>
2025-09-14 23:31:18 +0000

    changes made

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,20 +1,35 @@
+import { useParams, Link } from 'react-router-dom';
 import useRecipeStore from '../recipeStore';
+import EditRecipeForm from './EditRecipeForm';
+import DeleteRecipeButton from './DeleteRecipeButton';
 
-const RecipeDetails = ({ recipeId }) => {
-  const recipe = useRecipeStore((state) =>
-    state.recipes.find((r) => r.id === recipeId)
+const RecipeDetails = () => {
+  const { id } = useParams();    
+  const recipe = useRecipeStore((s) =>
+    s.recipes.find((r) => String(r.id) === String(id))
   );
 
-  if (!recipe) return <p>Recipe not found</p>;
+  if (!recipe) {
+    return (
+      <div style={{ padding: 20 }}>
+        <p>Recipe not found.</p>
+        <Link to="/">Back to list</Link>
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <h1>{recipe.title}</h1>
-      <p>{recipe.description}</p>
+    <div style={{ padding: 20, maxWidth: 600 }}>
+      <Link to=" / ">&larr; Back</Link>
+      <h1 style={{ marginTop: 8 }}>{recipe.title}</h1>
+      <p style={{ whiteSpace: 'pre-wrap' }}>{recipe.description}</p>
 
-      {/* Include edit + delete here */}
+      <h2 style={{ marginTop: 24 }}>Edit</h2>
       <EditRecipeForm recipe={recipe} />
-      <DeleteRecipeButton id={recipe.id} />
+
+      <div style={{ marginTop: 16 }}>
+        <DeleteRecipeButton id={recipe.id} />
+      </div>
     </div>
   );
 };
