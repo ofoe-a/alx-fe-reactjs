bf6b9dcf86e46042e6b4fb7a2e4c72f00c6ce5e7
ofoe <ofoe_98@hotmail.co.uk>
2025-09-28 20:01:17 +0000

    added p tag styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -7,7 +7,7 @@ function UserProfile() {
         className="rounded-full w-36 h-36 mx-auto"
       />
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-center">
+      <p className="text-gray-600 text-base text-center">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
