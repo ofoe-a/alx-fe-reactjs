261a99a36aad75295d4b5931cdfd273e89579f84
ofoe <ofoe_98@hotmail.co.uk>
2025-09-28 20:15:33 +0000

    added more styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,14 +1,14 @@
-// src/components/UserProfile.jsx
 export default function UserProfile() {
   return (
     <div
       className="
         bg-gray-100 
-        p-4 sm:p-4 md:p-8               
-        max-w-xs sm:max-w-xs md:max-w-sm  
-        mx-auto my-16                    
+        p-4 sm:p-4 md:p-8
+        max-w-xs md:max-w-sm
+        mx-auto my-16
         rounded-lg shadow-lg
         text-center
+        transition-shadow duration-300 hover:shadow-xl   
       "
     >
       <img
@@ -16,27 +16,28 @@ export default function UserProfile() {
         alt="User"
         className="
           rounded-full 
-          w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 
+          w-24 h-24 md:w-36 md:h-36
           mx-auto
+          transition-transform duration-300 ease-in-out  
+          hover:scale-110                                   
+          cursor-pointer
         "
       />
 
       <h1
         className="
           my-4
-          text-lg sm:text-lg md:text-xl
+          text-lg md:text-xl
           text-blue-800
+          transition-colors duration-200                    
+          hover:text-blue-500                               
+          cursor-default
         "
       >
         John Doe
       </h1>
 
-      <p
-        className="
-          text-gray-600
-          text-sm sm:text-sm md:text-base 
-        "
-      >
+      <p className="text-gray-600 text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
