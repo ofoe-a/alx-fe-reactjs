7eaf36bb33ed8511be3f9ec47f89fdc78573bf2a
ofoe <ofoe_98@hotmail.co.uk>
2025-10-11 17:24:59 +0000

    corrected search.jsx file by adding error code

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,19 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import RecipeDetail from "./components/RecipeDetail";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Home Page */}
         <Route path="/" element={<HomePage />} />
-
-        {/* Recipe Detail Page */}
         <Route path="/recipe/:id" element={<RecipeDetail />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   const [recipes, setRecipes] = useState([]);
