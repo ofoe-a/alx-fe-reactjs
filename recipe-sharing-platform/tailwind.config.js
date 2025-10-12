4a8cc596e8c85bb20db6ffaa3425502b3c7268d1
ofoe <ofoe_98@hotmail.co.uk>
2025-10-12 16:13:57 +0000

    edited tailwind.config.js so it includes public URL

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./public/index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
