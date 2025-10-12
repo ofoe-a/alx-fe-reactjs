21390738ff1dd51506b4b88d44b5bb35224b8494
ofoe <ofoe_98@hotmail.co.uk>
2025-10-12 21:49:24 +0000

    created formik form

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -49,7 +49,7 @@ export default function RegistrationForm() {
         {errors.username && <span style={styles.error}>{errors.username}</span>}
       </label>
 
-      <label>
+      <labe>
         Email
         <input
           name="email"
@@ -59,8 +59,7 @@ export default function RegistrationForm() {
           placeholder="you@example.com"
         />
         {errors.email && <span style={styles.error}>{errors.email}</span>}
-      </label>
-
+      </labe
       <label>
         Password
         <input
